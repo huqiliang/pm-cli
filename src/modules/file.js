bedf16cec1b8e711583a3ab42acdba19f35ec80d
胡奇良 <hql@ipms.cn>
2019-04-03 17:41:27 +0800

    feat: 项目基本结构

---
+++ src/modules/file.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+import path from "path";
+
+export default {
+  getCurrentDirectoryBase: () => {
+    return path.basename(process.cwd());
+  },
+
+  directoryExists: filePath => {
+    try {
+      return fs.statSync(filePath).isDirectory();
+    } catch (err) {
+      return false;
+    }
+  }
+};
