63b57ddf926c802ef5527ffcd2c3ca124c494190
胡奇良 <hql@ipms.cn>
2019-05-21 15:52:18 +0800

    feat: public config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   head: [
     ["link", { rel: "icon", href: "/favicon.ico" }] // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: "/", // 这是部署到github相关的配置 下面会讲
+  base: "/pm-cli/", // 这是部署到github相关的配置
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
