f7218ab55ad6c3c58d79751fdc9627b3cd3ecdec
胡奇良 <hql@ipms.cn>
2019-06-04 14:09:00 +0800

    fix: 调整命令格式

--- src/bin/pm-init.js
+++ src/bin/pm-init.js
@@ -2,12 +2,40 @@
 
 import program from "commander";
 import _ from "lodash";
-import git from "../modules/git";
+import fs from "fs";
+import file from "../modules/file";
+import inquirer from "inquirer";
+// import git from "../modules/git";
 
 const args = program.args;
-
-if (_.isEmpty(args)) {
-  program.help();
-} else {
-  git.clone(args);
+// console.log("====================================");
+// console.log(process.cwd());
+// console.log("====================================");
+// console.log(file.directoryExists(process.cwd()));
+// if (_.isEmpty(args)) {
+//   program.help();
+// } else {
+//   git.clone(args);
+// }
+async function init(path) {
+  console.log("====================================");
+  console.log("====================================");
+  if (file.directoryExists(path)) {
+    const a = await inquirer.prompt([
+      {
+        type: "confirm",
+        name: "toBeDelivered",
+        message: "存在目录，是否覆盖?",
+        default: false
+      }
+    ]);
+    // .then(function(answers) {
+    //   console.log("====================================");
+    //   console.log(answers);
+    //   console.log("====================================");
+    // });
+    console.log(a);
+  } else {
+  }
 }
+init(process.cwd());
